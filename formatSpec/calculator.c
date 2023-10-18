0f2284e7e951f55d76249d2d1f5cbb41eeacc454
frogwarlock <93387620+frogwarlock@users.noreply.github.com>
2023-10-18 18:36:34 -0300

    calculator the bad way

---
+++ formatSpec/calculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("a = %d, b = %d, sum = a + b = %d + %d = % d\n", 1, 2, 1, 2, 1+2);
+
+    printf("a = %d, b = %d, sum = a - b = %d - %d = % d\n", 2, 1, 2, 1, 2-1);
+
+    printf("a = %d, b = %d, multiply = a * b = %d * %d = % d\n", 1, 2, 1, 2, 1*2);
+
+    printf("a = %d, b = %d, divide = a / b = %d / %d = % d\n", 4, 2, 4, 2, 4/2);
+
+    printf("%d %% %d = %d \n", 5, 2, 5%2);
+}
